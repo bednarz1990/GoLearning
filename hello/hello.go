0f6268c61dae90583d84572749909803ae675f5a
Bednarczyk Lukasz <lukasz.bednarczyk@impaqgroup.com>
2018-09-07 20:02:37 +0200

    new commit

--- hello/hello.go
+++ hello/hello.go
@@ -1,10 +1,23 @@
 package main
-import (
-    "fmt"
-    "os"
-    "strings"
-)
-func main() {
-    fmt.Println(strings.Join(os.Args[1:], " "))
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+
+	fmt.Printf("ala ma kotka2")
+	// NOTE: Ignoring potential errors form input.Error()
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d, %s\n", n, line)
+		}
+	}
 }
